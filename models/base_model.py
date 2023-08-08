0716adae41638a057b26deb867f7b2461d8d0668
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-08-08 21:17:41 +0300

    update def initialization function handle id, created_at and  updated_at attrs def str method for the str representation

--- models/base_model.py
+++ models/base_model.py
@@ -8,4 +8,15 @@ from datetime import date, datetime
 
 class BaseModel:
     """defines all common attributes/methods for other classes"""
-    pass
+
+    def __init__(self):
+        """initializes a new instance of base model"""
+        self.id = str(uuid.uuid4())
+        tnow = datetime.now()
+        self.created_at = tnow
+        self.updated_at = tnow
+
+    def __str__(self):
+        """returns a string representation of an instance"""
+        name = self.__class__.__name__
+        return f"[{name}] ({self.id}) {self.__dict__}"
