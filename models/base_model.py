21dcc42612646487cf05d1a5d82de86b4be4635e
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-08-08 20:49:48 +0300

    create basemodel class (the base of every thing) make a test file for base model

---
+++ models/base_model.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Define a class BaseModel that defines all common attributes/methods for other classes"""
+
+import uuid
+from datetime import date, datetime
+
+
+class BaseModel:
+    """defines all common attributes/methods for other classes"""
+    pass
